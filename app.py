4205d6ee1b2954a615f5b99e22200e5765ed86bc
Pierre Angot <pierre.angot@verteego.com>
2018-11-05 14:22:36 +0100

    back to endchain

--- app.py
+++ app.py
@@ -1,8 +1,53 @@
+#https://github.com/datademofun/heroku-basic-flask
+#https://help.pythonanywhere.com/pages/Flask
+
 from flask import Flask
 from flask_restful import Api, Resource, reqparse
+from flask import send_file
+
+import pyqrcode
+import barcode
+from matplotlib.pyplot import imshow
+import numpy as np
+from PIL import Image
 
 app = Flask(__name__)
 api = Api(app)
+
+
+def generate( qrAddress, barCode ):
+    # Generate EAN13 Barcode
+    '''
+    from barcode.writer import ImageWriter
+    EAN = barcode.get_barcode_class('ean13')
+    ean = EAN(barCode, writer=ImageWriter())
+    fullname = ean.save('ean13_barcode')
+    '''
+    #Crop the Barcode
+    im=Image.open("ean13_barcode.png")
+    #print im.size
+    im2=im.crop((65, 0, 460, 200))
+    #print im2.size
+    im2.save("ean13_barcode_cropped.png")
+
+    #Generate QrCode
+    url = pyqrcode.create(qrAddress,error = 'H', version=5)
+    url.png('generatedQR.png',scale=10)
+
+    #Combine the Barcode and QRcode
+    im = Image.open('generatedQR.png')
+    im = im.convert("RGBA")
+    logo = Image.open('ean13_barcode_cropped.png')
+    box = (190,200,450,310)
+    im.crop(box)
+    region = logo
+    region = region.resize((box[2] - box[0], box[3] - box[1]))
+    im.paste(region,box)
+    im.show()
+
+    #Save the combination
+    im.save('out.png')
+    return True
 
 users = [
     {
@@ -23,56 +68,15 @@ users = [
 ]
 
 class User(Resource):
-    def get(self, name):
-        for user in users:
-            if(name == user["name"]):
-                return user, 200
-        return "User not found", 404
+    def get(self, barcode, url):
+        if(barcode and url):
+            generate(url, barcode)
+            return send_file('out.png', mimetype='image/png')
+        return "Forbidden", 404
 
-    def post(self, name):
-        parser = reqparse.RequestParser()
-        parser.add_argument("age")
-        parser.add_argument("occupation")
-        args = parser.parse_args()
 
-        for user in users:
-            if(name == user["name"]):
-                return "User with name {} already exists".format(name), 400
+api.add_resource(User, "/barcode/<string:barcode>/url/<string:url>")
 
-        user = {
-            "name": name,
-            "age": args["age"],
-            "occupation": args["occupation"]
-        }
-        users.append(user)
-        return user, 201
+app.run(debug=True)
 
-    def put(self, name):
-        parser = reqparse.RequestParser()
-        parser.add_argument("age")
-        parser.add_argument("occupation")
-        args = parser.parse_args()
-
-        for user in users:
-            if(name == user["name"]):
-                user["age"] = args["age"]
-                user["occupation"] = args["occupation"]
-                return user, 200
-
-        user = {
-            "name": name,
-            "age": args["age"],
-            "occupation": args["occupation"]
-        }
-        users.append(user)
-        return user, 201
-
-    def delete(self, name):
-        global users
-        users = [user for user in users if user["name"] != name]
-        return "{} is deleted.".format(name), 200
-
-api.add_resource(User, "/user/<string:name>")
-
-if __name__ == '__main__':
-    app.run()
+#http://127.0.0.1:5000/barcode/5901234123457/url/kdsjflsdkjfsd
